ab57dcf1895664fad9d449395ecebc27e9888056
Bruno Araújo <brunooliveiraaraujo@hotmail.com>
2020-03-22 16:59:20 -0300

    Criando Dao e Business para categoria

---
+++ src/main/java/com/cursomc/business/CategoriaBusiness.java
@@ -0,0 +1,22 @@
+package com.cursomc.business;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.domain.Categoria;
+import com.cursomc.repositories.CategoriaDao;
+
+@Service
+public class CategoriaBusiness {
+	
+	@Autowired
+	private CategoriaDao categoriaDao; 
+	
+	public Categoria buscar(Integer id) {
+		 Optional<Categoria> obj = categoriaDao.findById(id);
+		 return obj.orElse(null);
+	}
+
+}
--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -3,27 +3,29 @@ package com.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursomc.business.CategoriaBusiness;
 import com.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaBusiness categoriaBusiness;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> findById(@PathVariable Integer id) {
+		Categoria obj = categoriaBusiness.buscar(id);
 		
-		Categoria cat = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
