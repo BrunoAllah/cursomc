ef08f7d39b5b24d9da0d9f0b04e0b39c680747c4
Bruno Araújo <brunooliveiraaraujo@hotmail.com>
2020-03-23 22:36:17 -0300

    Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cursomc.domain.Categoria;
 import com.cursomc.domain.Cidade;
+import com.cursomc.domain.Cliente;
+import com.cursomc.domain.Endereco;
 import com.cursomc.domain.Estado;
 import com.cursomc.domain.Produto;
+import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.repositories.CategoriaDao;
 import com.cursomc.repositories.CidadeDao;
+import com.cursomc.repositories.ClienteDao;
+import com.cursomc.repositories.EnderecoDao;
 import com.cursomc.repositories.EstadoDao;
 import com.cursomc.repositories.ProdutoDao;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeDao cidadeDao;
+	
+	@Autowired
+	private ClienteDao clienteDao;
+	
+	@Autowired
+	private EnderecoDao enderecoDao;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -68,6 +79,16 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoDao.saveAll(Arrays.asList(est1, est2));
 		cidadeDao.saveAll(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria", "maria@gmail.com", "00000000", TipoCliente.PESSOA_FISICA);
+		cli1.getTelefones().addAll(Arrays.asList("1100000000", "1111111111"));
+		
+		Endereco e1 = new Endereco(null, "Av. Paulista", "1500", "Apto 23", "Jardins", "00000-000", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av. Paulista", "3425", "Apto 76", "Consolação", "00000-000", cli1, c2);		
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));		
+		
+		clienteDao.save(cli1);
+		enderecoDao.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
