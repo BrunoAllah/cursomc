b60496c73eb1d2451942420bd5d2081846e95f0d
Bruno Araújo <brunooliveiraaraujo@hotmail.com>
2020-03-31 17:27:51 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.cursomc.domain.Cidade;
 import com.cursomc.domain.Cliente;
 import com.cursomc.domain.Endereco;
 import com.cursomc.domain.Estado;
+import com.cursomc.domain.Pagamento;
+import com.cursomc.domain.PagamentoComBoleto;
+import com.cursomc.domain.PagamentoComCartao;
+import com.cursomc.domain.Pedido;
 import com.cursomc.domain.Produto;
+import com.cursomc.domain.enums.EstadoPagamento;
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.repositories.CategoriaDao;
 import com.cursomc.repositories.CidadeDao;
 import com.cursomc.repositories.ClienteDao;
 import com.cursomc.repositories.EnderecoDao;
 import com.cursomc.repositories.EstadoDao;
+import com.cursomc.repositories.PagamentoDao;
+import com.cursomc.repositories.PedidoDao;
 import com.cursomc.repositories.ProdutoDao;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoDao enderecoDao;
+	
+	@Autowired
+	private PedidoDao pedidoDao;
+	
+	@Autowired
+	private PagamentoDao pagamentoDao;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,6 +103,22 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteDao.save(cli1);
 		enderecoDao.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:30"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoDao.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoDao.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
---
+++ src/main/java/com/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,36 @@
+package com.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+
+	PENDENTE(1, "Pendente"),
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+	
+	private int cod;
+	private String desc;
+	
+	private EstadoPagamento(int cod, String desc) {
+		this.cod = cod;
+		this.desc = desc;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+	
+	public static EstadoPagamento toEnum(Integer cod) {
+		if (cod == null)
+			return null;
+		
+		for (EstadoPagamento estadoPagamento : EstadoPagamento.values()) {
+			if (cod.equals(estadoPagamento.getCod()))
+				return estadoPagamento;
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+	
+}
