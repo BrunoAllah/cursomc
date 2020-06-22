4fa8452cc0ea93f4a510f19ed67e6953451f937f
Bruno Araújo <brunooliveiraaraujo@hotmail.com>
2020-06-22 17:14:13 -0300

    Criando profile de teste

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -1,169 +1,18 @@
 package com.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cursomc.domain.Categoria;
-import com.cursomc.domain.Cidade;
-import com.cursomc.domain.Cliente;
-import com.cursomc.domain.Endereco;
-import com.cursomc.domain.Estado;
-import com.cursomc.domain.ItemPedido;
-import com.cursomc.domain.Pagamento;
-import com.cursomc.domain.PagamentoComBoleto;
-import com.cursomc.domain.PagamentoComCartao;
-import com.cursomc.domain.Pedido;
-import com.cursomc.domain.Produto;
-import com.cursomc.domain.enums.EstadoPagamento;
-import com.cursomc.domain.enums.TipoCliente;
-import com.cursomc.repositories.CategoriaDao;
-import com.cursomc.repositories.CidadeDao;
-import com.cursomc.repositories.ClienteDao;
-import com.cursomc.repositories.EnderecoDao;
-import com.cursomc.repositories.EstadoDao;
-import com.cursomc.repositories.ItemPedidoDao;
-import com.cursomc.repositories.PagamentoDao;
-import com.cursomc.repositories.PedidoDao;
-import com.cursomc.repositories.ProdutoDao;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaDao categoriaDao;
-	
-	@Autowired
-	private ProdutoDao produtoDao;
-	
-	@Autowired
-	private EstadoDao estadoDao;
-	
-	@Autowired
-	private CidadeDao cidadeDao;
-	
-	@Autowired
-	private ClienteDao clienteDao;
-	
-	@Autowired
-	private EnderecoDao enderecoDao;
-	
-	@Autowired
-	private PedidoDao pedidoDao;
-	
-	@Autowired
-	private PagamentoDao pagamentoDao;
-	
-	@Autowired
-	private ItemPedidoDao itemPedidoDao;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Cama, Mesa e Banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
-		Categoria cat5 = new Categoria(null, "Jardinagem");
-		Categoria cat6 = new Categoria(null, "Decoração");
-		Categoria cat7 = new Categoria(null, "Perfumária");
-		
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
-		Produto p5 = new Produto(null, "Toalha", 50.00);
-		Produto p6 = new Produto(null, "Colcha", 200.00);
-		Produto p7 = new Produto(null, "SmartTV", 2500.00);
-		Produto p8 = new Produto(null, "Cadeira", 320.00);
-		Produto p9 = new Produto(null, "Abajour", 100.00);
-		Produto p10 = new Produto(null, "Pendente", 180.00);
-		Produto p11 = new Produto(null, "Shampoo", 40.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
-		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
-		cat5.getProdutos().addAll(Arrays.asList(p8));
-		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
-		cat7.getProdutos().addAll(Arrays.asList(p11));
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p4.getCategorias().addAll(Arrays.asList(cat2));
-		p5.getCategorias().addAll(Arrays.asList(cat3));
-		p6.getCategorias().addAll(Arrays.asList(cat3));
-		p7.getCategorias().addAll(Arrays.asList(cat4));
-		p8.getCategorias().addAll(Arrays.asList(cat5));
-		p9.getCategorias().addAll(Arrays.asList(cat6));
-		p10.getCategorias().addAll(Arrays.asList(cat6));
-		p11.getCategorias().addAll(Arrays.asList(cat7));
-		
-		categoriaDao.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		produtoDao.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
-		
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-		
-		Cidade c1 = new Cidade(null, "Uberlândia", est1);
-		Cidade c2 = new Cidade(null, "São Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
-		est1.getCidades().add(c1);
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
-		estadoDao.saveAll(Arrays.asList(est1, est2));
-		cidadeDao.saveAll(Arrays.asList(c1, c2, c3));
-		
-		Cliente cli1 = new Cliente(null, "Maria", "maria@gmail.com", "00000000", TipoCliente.PESSOA_FISICA);
-		cli1.getTelefones().addAll(Arrays.asList("1100000000", "1111111111"));
-		
-		Endereco e1 = new Endereco(null, "Av. Paulista", "1500", "Apto 23", "Jardins", "00000-000", cli1, c1);
-		Endereco e2 = new Endereco(null, "Av. Paulista", "3425", "Apto 76", "Consolação", "00000-000", cli1, c2);		
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));		
-		
-		clienteDao.save(cli1);
-		enderecoDao.saveAll(Arrays.asList(e1, e2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm");
-		
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:30"), cli1, e2);
-		
-		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		ped1.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		ped2.setPagamento(pagto2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
-		pedidoDao.saveAll(Arrays.asList(ped1, ped2));
-		pagamentoDao.saveAll(Arrays.asList(pagto1, pagto2));
-		
-		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
-		
-		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
-		ped2.getItens().add(ip3);
-		
-		p1.getItens().add(ip1);
-		p2.getItens().add(ip3);
-		p3.getItens().add(ip2);
-		
-		itemPedidoDao.saveAll(Arrays.asList(ip1, ip2, ip3));
-		
 	}
 
 }
--- src/main/java/com/cursomc/config/TestConfig.java
+++ src/main/java/com/cursomc/config/TestConfig.java
@@ -1,15 +1,28 @@
 package com.cursomc.config;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursomc.business.DatabaseBusiness;
 import com.cursomc.business.EmailBusiness;
 import com.cursomc.business.MockEmailBusiness;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
+	
+	@Autowired
+	private DatabaseBusiness dbBusiness;
+	
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		dbBusiness.instantiateTestDatabase();
+		return true;
+	}
 	
 	@Bean
 	public EmailBusiness emailBusiness() {
